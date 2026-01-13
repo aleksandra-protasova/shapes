03b1294a2a3a24b6d863acedbabe5e480b010847
aleksandra-protasova <sanny_2006@mail.ru>
2026-01-13 14:17:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='geometric_shapes',
     version='0.0.1',
     packages=find_packages("."),
-    scripts=["bin/shapes_app"],
+    scripts=["bin/shapes_app.py"],
     url='https://github.com/aleksandra-protasova/protasova_av',
     license='Apache-2.0',
     author='Протасова Александра Владимировна',
@@ -14,4 +14,5 @@ setup(
     install_requires=[
         'tabulate>=0.8.9',
     ],
+
 )
